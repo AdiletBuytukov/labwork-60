1bb85059e546e1f85770d6883ec0a29af5ff78e2
Adilet Buytukov <abuytukov@gmail.com>
2024-01-30 15:00:22 +0600

    Добавлен функционал для отправки нового сообщения сообщения.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import MessageList from "./components/MessageList";
-import {useState} from "react";
-import Message from "./components/Message";
+import React, {useState} from "react";
 import MessageForm from "./components/MessageForm";
 
 function App() {
@@ -9,16 +8,37 @@ function App() {
   const [message, setMessage] = useState('');
   const [messagesList, setMessagesList] = useState<{author: string; message: string}[]>([]);
 
+  const formSubmit = async (e: React.FormEvent<HTMLFormElement>)=> {
+    e.preventDefault()
+
+    const body = new URLSearchParams();
+    body.append('author', author);
+    body.append('message', message);
+
+    try {
+      const response = await fetch('http://146.185.154.90:8000/messages', {method: 'POST', body});
+      const newMessage = await response.json();
+
+      setMessagesList((prevMessage) => [...prevMessage, newMessage]);
+
+      setAuthor('');
+      setMessage('');
+
+    } catch (error) {
+      console.log('Reason: ', error)
+    }
+  }
 
   return (
     <>
-      <Message
+      <MessageForm
+        onSubmit={formSubmit}
         author={author}
         message={message}
+        changeAuthor={(e) => setAuthor(e.target.value)}
+        changeMessage={(e) => setMessage(e.target.value)}
       />
       <MessageList messagesList={messagesList}/>
-      App
-      <MessageForm/>
     </>
   )
 }
